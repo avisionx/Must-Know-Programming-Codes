d870261a7d032a28f78bf515068a5744c8b344d1
vaibhavpulastya <39432327+vaibhavpulastya@users.noreply.github.com>
2018-10-29 23:31:36 +0530

    Added description

--- Arrays/pre_sum.py
+++ Arrays/pre_sum.py
@@ -1,3 +1,8 @@
+""" 
+preSum is the prefix sum array, preSum[i] --> sum of elements of array from index 0 to i, i.e. sum of elements of prefix of length i of array. 
+for eg. - 
+	for, array = [1,2,3] the output array, preSum = [1,3,6]
+"""
 array = list(map(int,input().split()))
 preSum = []
 preSum.append(array[0])
