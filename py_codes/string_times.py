6b38064f9276bb3b2966b4fc1c9fb4cdc1391045
avi-garg <44573288+avi-garg@users.noreply.github.com>
2019-10-14 02:10:30 +0530

    Update string_times.py

--- py_codes/string_times.py
+++ py_codes/string_times.py
@@ -1,10 +1,9 @@
-
-'''Given a string and a non-negative int n, return a larger string that is n copies of the original string.
-
-
-string_times('Hi', 2) → 'HiHi'
-string_times('Hi', 3) → 'HiHiHi'
-string_times('Hi', 1) → 'Hi' '''
+"""
+  Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+  string_times('Hi', 2) → 'HiHi'
+  string_times('Hi', 3) → 'HiHiHi'
+  string_times('Hi', 1) → 'Hi'
+"""
 
 def string_times(given_str, times):
   print_str = '' 
@@ -13,6 +12,7 @@ def string_times(given_str, times):
     times -= 1
   return print_str
   
-given_str = "Hi"
-times = 3
-print(string_times(given_str,times))
+if __name__ == "__main__":
+  given_str = "Hi"
+  times = 3
+  print(string_times(given_str,times))
